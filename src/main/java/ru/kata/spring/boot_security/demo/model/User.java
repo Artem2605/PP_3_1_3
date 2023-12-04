ad88a369bc6ae3710ed8550d2504f879f2c044de
Artem2605 <artyom260599@mail.ru>
2023-12-04 13:41:33 +0300

    4

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -37,7 +37,7 @@ public class User implements UserDetails {
     @NotNull(message = "Неверно указан год")
     private Long year;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
